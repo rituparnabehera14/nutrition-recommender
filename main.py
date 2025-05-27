3684981246c63cb0d170f2eb9a10c1ae2be39330
Rituparna Behera <rituparnabehera007@gmail.com>
2025-05-27 20:58:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-# main.py
-
 from src.preprocess import load_and_clean_data
 from src.cluster import cluster_foods
 from src.recommend import recommend_foods
-
-
-# Load and clean data
 df = load_and_clean_data("data")
 print(df.head())
-
 df_clustered = cluster_foods(df, n_clusters=5)
-
-
-
-# Cluster foods by nutrition
 df = cluster_foods(df)
-
-# Recommend based on a nutritional goal
 recommend_foods(df, goal='high protein')
 print(df.columns.tolist())
 
